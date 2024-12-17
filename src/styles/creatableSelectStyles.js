13e0bbd7f1a1a394fc32ca6c2439e8286d1d48f3
PiotrekSiwek <66331663+PiotrekSiwek@users.noreply.github.com>
2024-12-17 20:14:54 +0100

    styles

---
+++ src/styles/creatableSelectStyles.js
@@ -0,0 +1,17 @@
+export const selectStyles = {
+  placeholder: (provided) => ({
+    ...provided,
+    fontStyle: "italic",
+    fontFamily: "system-ui",
+    color: "#6c757d",
+  }),
+  control: (provided, state) => ({
+    ...provided,
+    border: state.isFocused ? "1px solid #007bff" : "1px solid #dee2e6",
+    borderRadius: "0.25rem",
+    boxShadow: state.isFocused
+      ? "0 0 0 0.2rem rgba(0, 123, 255, 0.25)"
+      : "none",
+    padding: "6px 6px",
+  }),
+};
